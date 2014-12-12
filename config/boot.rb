2db76d0ec8b4d416af9e1b3d359bd93611aa8c1b
ogawa <z00s109031@gmail.com>
2014-12-12 10:23:19 +0900

    Changed logo image and default port for launch

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,13 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# developmentのdefault portを3456番にする
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge( { :Port => 3456 } )
+    end
+  end
+end
